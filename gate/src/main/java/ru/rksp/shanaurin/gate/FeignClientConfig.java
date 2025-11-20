642c13bb2f9b33081e2d01e2fb7029241e007b79
Anton A Shanaurin <stud0000298878@utmn.ru>
2025-11-20 16:18:57 +0300

    gate conroller

--- gate/src/main/java/ru/rksp/shanaurin/gate/FeignClientConfig.java
+++ gate/src/main/java/ru/rksp/shanaurin/gate/FeignClientConfig.java
@@ -1,5 +1,6 @@
 package ru.rksp.shanaurin.gate;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.rksp.shanaurin.gate.client.ApiClient;
@@ -7,12 +8,14 @@ import ru.rksp.shanaurin.gate.client.api.StudentDataApi;
 
 @Configuration
 public class FeignClientConfig {
+    @Value("${data.service.url}")
+    private String dataServiceUrl;
 
     @Bean
     public StudentDataApi studentDataApi() {
         ApiClient apiClient = new ApiClient();
         // адрес сервиса data-service
-        apiClient.setBasePath("http://localhost:8083");
+        apiClient.setBasePath(dataServiceUrl);
         return apiClient.buildClient(StudentDataApi.class);
     }
 }
---
+++ gate/src/main/java/ru/rksp/shanaurin/gate/StudentGateController.java
@@ -0,0 +1,38 @@
+package ru.rksp.shanaurin.gate;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import ru.rksp.shanaurin.data.api.StudentGateApi;
+import ru.rksp.shanaurin.data.model.StudentGateCreateRequest;
+import ru.rksp.shanaurin.data.model.StudentGateResponse;
+import ru.rksp.shanaurin.gate.client.api.StudentDataApi;
+import ru.rksp.shanaurin.gate.client.model.StudentDataCreateRequest;
+import ru.rksp.shanaurin.gate.client.model.StudentDataResponse;
+
+@RestController
+@RequiredArgsConstructor
+public class StudentGateController implements StudentGateApi {
+
+    private final StudentDataApi studentsFeignClient;
+
+    /**
+     * Проксирует создание студента во внутренний DATA-SERVICE.
+     * Получает запрос от клиента, преобразует модель и перенаправляет.
+     */
+    @Override
+    public ResponseEntity<StudentGateResponse> createStudent(StudentGateCreateRequest request) {
+        StudentDataCreateRequest dataRequest = new StudentDataCreateRequest();
+        dataRequest.setFullName(request.getFullName());
+        dataRequest.setPassport(request.getPassport());
+
+        StudentDataResponse dataResponse = studentsFeignClient.createStudentDataInData(dataRequest);
+
+        StudentGateResponse gateResponse = new StudentGateResponse();
+        gateResponse.setId(dataResponse.getId());
+        gateResponse.setFullName(dataResponse.getFullName());
+        gateResponse.setPassport(dataResponse.getPassport());
+
+        return ResponseEntity.status(201).body(gateResponse);
+    }
+}
