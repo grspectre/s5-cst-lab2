8c4825abe53d821bfdcc08e7e959901f401e2b44
Anton A Shanaurin <stud0000298878@utmn.ru>
2025-11-20 12:44:20 +0300

    create method in controller

--- src/main/java/ru/rksp/shanaurin/data_service/StudentController.java
+++ src/main/java/ru/rksp/shanaurin/data_service/StudentController.java
@@ -1,4 +1,35 @@
 package ru.rksp.shanaurin.data_service;
 
-public class StudentController {
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import ru.rksp.shanaurin.data.api.StudentDataApi;
+import ru.rksp.shanaurin.data.model.StudentDataCreateRequest;
+import ru.rksp.shanaurin.data.model.StudentDataResponse;
+
+@RestController
+@RequiredArgsConstructor
+public class StudentController implements StudentDataApi {
+
+    private final StudentRepository studentRepository;
+
+    @Override
+    public ResponseEntity<StudentDataResponse> createStudentDataInData(StudentDataCreateRequest request) {
+        Student student = new Student();
+        student.setName(request.getFullName());
+        student.setPassport(request.getPassport());
+        studentRepository.save(student);
+        StudentDataResponse response = new StudentDataResponse();
+        response.setId(student.getId());
+        response.setFullName(student.getName());
+        response.setPassport(student.getPassport());
+
+        return ResponseEntity.status(200).body(response);
+    }
+
+  /*@Override
+  public ResponseEntity<StudentDataResponse> getStudentDataByIdFromData(Long id) {
+
+    return ResponseEntity.status(200).body(response);
+  }*/
 }
